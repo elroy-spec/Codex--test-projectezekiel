cf2a05a3bf8ed6eb0dccdc55decb1bf784513a17
elroy-spec <elroy.9008523151@gmail.com>
2026-04-16 16:30:55 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -12,6 +12,25 @@ app.get("/topics", (req, res) => {
     topics: ["physics", "chemistry", "biology", "math"]
   });
 });
+app.get("/question", (req, res) => {
+  res.json({
+    topic: "math",
+    question: "What is 5 + 3?",
+    options: [5, 6, 7, 8],
+    answer: 8
+  });
+});
+app.post("/check", (req, res) => {
+  const { answer } = req.body;
+
+  const correctAnswer = 8;
+
+  if (answer == correctAnswer) {
+    res.json({ correct: true });
+  } else {
+    res.json({ correct: false });
+  }
+});
 app.listen(PORT, "0.0.0.0", () => {
   console.log("App is running on port " + PORT);
 });
