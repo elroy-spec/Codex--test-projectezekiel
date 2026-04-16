89d1775f72f53677d58f02c29763fc7edc00c2ea
elroy-spec <elroy.9008523151@gmail.com>
2026-04-16 15:31:00 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 
@@ -6,6 +5,8 @@ app.get("/", (req, res) => {
   res.send("Server running");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
 });
