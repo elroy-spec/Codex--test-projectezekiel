b59bd0ba1a388063bcd43d5437bde1cb4ab68056
elroy-spec <elroy.9008523151@gmail.com>
2026-04-16 16:28:49 +0530

    Update Index.js

--- Backend/Index.js
+++ Backend/Index.js
@@ -7,7 +7,11 @@ app.get("/", (req, res) => {
 
 // IMPORTANT: Replit-safe binding
 const PORT = process.env.PORT || 3000;
-
+app.get("/topics", (req, res) => {
+  res.json({
+    topics: ["physics", "chemistry", "biology", "math"]
+  });
+});
 app.listen(PORT, "0.0.0.0", () => {
   console.log("App is running on port " + PORT);
 });
